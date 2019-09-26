708f71e12307b7def1b360b5e5be28f929930daa
Tyler Weatherby <teweatherby@gmail.com>
2019-09-26 15:48:48 -0400

    Comments
    Commented code and cleaned up.

---
+++ ReverseInteger/ReverseInteger.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class ReverseInteger {
+     public static int reverse(int x)
+     {
+          // result needs to be type long incase an integer > [2^31 - 1 or -2^31] was passed
+          long result = 0;
+          
+          // Using just arithmetic operators, reverse the integer passed through.
+          while (x != 0)
+          {
+               result *= 10;
+               result += x % 10;
+               x /= 10;
+          }
+          // Check to see if our long is within integer range. [2^31 - 1 or -2^31]
+          if (result > (Math.pow(2, 31) - 1) || result < (Math.pow(2, 31) * -1))
+          {
+               // Specified if overflowed to return 0.
+               return 0;
+          }
+          // Convert long to int
+          return (int)result;
+    }
+}
